df92f4ab7f0715e4877a35a608a552b1849029ab
Gabriel <ga-briel.t.a@hotmail.com>
2024-11-20 21:30:28 -0300

    definindo cores ao iniciar o app

--- lib/controllers/app_controller.dart
+++ lib/controllers/app_controller.dart
@@ -17,11 +17,26 @@ class AppController extends ChangeNotifier {
 }
 
 class MultiPlayerAppController extends ChangeNotifier {
+  Map<String, List<Color?>> appColors = {
+    'green': [Colors.green[300], Colors.green[400]],
+    'purple': [Colors.purple[300], Colors.purple[400]],
+    'cyan': [Colors.cyan[300], Colors.cyan[400]],
+  };
   List<AppController> players = [];
 
   MultiPlayerAppController(int numJogadores) {
     for (int i = 0; i < numJogadores; i++) {
       final player = AppController();
+      switch (i) {
+        case 0:
+          player.lifeColor = appColors['green']?[0];
+          player.bgColor = appColors['green']?[1];
+          break;
+        case 1:
+          player.lifeColor = appColors['purple']?[0];
+          player.bgColor = appColors['purple']?[1];
+          break;
+      }
       player.addListener(notifyListeners);
       players.add(player);
     }
